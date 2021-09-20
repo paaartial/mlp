7775ed1dddf0a183c5284dcd853bad864212bc94
Tom <tccatalan7@gmail.com>
2021-09-19 19:39:14 -0700

    cleaner

--- run.py
+++ run.py
@@ -25,23 +25,16 @@ def load_network(network_name, new_name=""):
     return network_to_load
 
 mnist = tf.keras.datasets.mnist.load_data()
-mnist_train_data, mnist_train_target, mnist_test_data, mnist_test_target = conv(mnist[0][0]), mnist[0][1], conv(mnist[1][0]), mnist[1][1]
-mnist_train, mnist_test = mnist[0], mnist[1]
+mnist_train, mnist_test = (conv(mnist[0][0]), mnist[0][1]), (conv(mnist[1][0]), mnist[1][1])
 
-train_size=100
-test_size=100
+train_size = 100
+test_size = 1
 
+#print(fashion_mnist_train_data[0])
 to_train, to_test = split_train_test(mnist_train, mnist_test, train_size, test_size)
-# ADD MORE WAYS TO EVALUATE NETWORK PERFORMANCE
-# SPLIT TRAIN TEST
-# FEED FORWARD, TRAIN, TEST TAKE TUPLE OF DATA AND TARGET
-
-test_net=Net("test_net", [mnist_train_data[0].size, 128, 10], 0.05)
-
+test_net=Net("test_net", [mnist_train[0][0].size, 128, 10], 0.05)
 test_net.train(to_train)
 test1=test_net.test(to_test)
 print(test1)
-#test_2 = nn.test(mnist_data_train, mnist_target_train, test_indices)
-#print(test_2)
 
 test_list=[1, 4, 2, 10, 5]
