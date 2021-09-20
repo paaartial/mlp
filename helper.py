6474a360b5e11e9e2d31699dfed3aa6d28c363be
Tom <tccatalan7@gmail.com>
2021-09-19 19:38:25 -0700

    moved over cost/activation/dataset functions

--- helper.py
+++ helper.py
@@ -3,6 +3,9 @@ import json
 
 import numpy as np
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 #COST
 def mean_squared_error(predict, target):
     target_list=[0 for i in range(10)]
@@ -32,3 +35,18 @@ def split_train_test(train, test, train_size, test_size):
     to_train_pairs = [(train[0][shuffled_index], train[1][shuffled_index]) for shuffled_index in range(len(shuffled_train_indices))]
     to_test_pairs = [(test[0][shuffled_index], test[1][shuffled_index]) for shuffled_index in range(len(shuffled_test_indices))]
     return (to_train_pairs, to_test_pairs)
+
+#RANDOM SHIT
+def draw_image(img_to_draw):
+    some_digit_image = img_to_draw.reshape(28, 28)
+    plt.imshow(some_digit_image, cmap = mpl.cm.binary, interpolation="nearest")
+    plt.axis("off")
+    plt.show()
+
+def element_wise_mult(l1, l2):
+    try:
+        assert len(l1) == len(l2)
+    except:
+        print("length of lists are not equal")
+
+    return [l1[n]*l2[n] for n in range(len(l1))]
