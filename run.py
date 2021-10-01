df9eaf3f07487dd055d3f9c1a9fddb3437961601
Tom <tccatalan7@gmail.com>
2021-09-30 17:37:58 -0700

    rename network class

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from layer import Layer
-from network import Net
+from network import Network
 
 from helper import *
 
@@ -9,7 +9,7 @@ import skimage.measure
 import numpy as np
 import pandas as pd
 
-np.random.seed(42)
+np.random.seed(69)
 
 def conv(input):
     return skimage.measure.block_reduce(input, (1,3,3), np.max)
@@ -19,7 +19,7 @@ def load_network(network_name, new_name=""):
         new_name=network_name
     with open(network_name +'.json', 'r') as openfile:
         network_data = json.load(openfile)
-        network_to_load = Net(new_name, network_data["layers"], network_data["lr"])
+        network_to_load = Network(new_name, network_data["layers"], network_data["lr"])
         network_to_load.set_weights(network_data["weights"]) 
     return network_to_load
 
@@ -35,16 +35,20 @@ def find_optimal_learning_rate(start, delta, num_tests):
     performance_by_lr = {}
     for iteration in range(1, num_tests+1):
         lr=start+delta*iteration
-        temp_net=Net("temp_net", [mnist_train[0][0].size, 76, 10], lr)
+        temp_net=Network("temp_net", [mnist_train[0][0].size, 76, 10], lr)
         temp_net.train(to_train, track_progress=False)
         test=temp_net.test(to_test)
         print("test " + str(iteration) + " done")
         performance_by_lr[lr] = test
     return performance_by_lr
 
-test_net = Net("test_net", [mnist_train[0][0].size, 76, 10], 0.01)
-test_net.train(to_train)
+#tests = find_optimal_learning_rate(0.005, 0.001, 50)
+#print(sort_things(tests))
+
+#0.055
+"""num_epochs=5
+test_net = Network("test_net", [mnist_train[0][0].size, 76, 10], 0.015)
+for epoch in range(num_epochs):
+    test_net.train(to_train)
 test1=test_net.test(to_test)
-print(test1)
-#tests=find_optimal_learning_rate(0.009, 0.001, 10)
-#print(tests)
+print(test1)"""
